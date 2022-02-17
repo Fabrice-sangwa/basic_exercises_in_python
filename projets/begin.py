d69eaa0d6a5cebf7cf346457153d8b18db5dc253
Fabrice sangwa <19sm476@esisalama.org>
2022-02-17 21:40:31 +0200

    Simples exercices en python

---
+++ projets/begin.py
@@ -0,0 +1,30 @@
+#Afficher un message 
+
+from cmath import sqrt
+
+
+print("Hello, word")
+
+#Afficher une message sur plusieurs lignes
+
+print("I am a beginner\nI need python")
+
+#Des simples opérations Arithmétiques avec Python
+
+nb1 = 2
+nb2 = 4
+sum = nb1 + nb2
+diff = nb2 - nb1
+div = nb2 / nb1
+mult = nb1 * nb2
+power = nb1 ** nb2
+
+print("Somme = ", sum,"Reste = ", diff,"quotient = ", div,"produit = ",mult, "Puissance = ", power)
+
+#Des opérations artmétiques supllémentaires
+
+modulo = nb2%nb2
+
+div_ent = nb2//nb1
+
+print("Modulo = ", modulo, "Division entière = ", div_ent)
